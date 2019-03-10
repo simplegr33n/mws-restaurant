dc6766f33332fd94bb89f8e8deaf4c076f4f1c74
Geoff Golda <gcgolda@gmail.com>
2019-03-09 23:52:54 -0700

    added alt text to main restaurant image in main.js as per review

--- js/main.js
+++ js/main.js
@@ -148,6 +148,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = ""
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.srcset = DBHelper.imageSrcsetForRestaurant(restaurant);
   image.sizes = "300px";
