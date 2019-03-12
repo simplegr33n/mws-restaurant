2cea800b8f5fd5b8bd05dcd3770cf24ce45e88ea
Geoff Golda <gcgolda@gmail.com>
2019-03-12 14:59:43 -0600

    added package.json, removed grunt, added babel and terser modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 const gulp = require('gulp');
+const size = require('gulp-size');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
@@ -21,24 +24,27 @@ const paths = {
 
 // UTILITY - GULP TASKS
 // HTML output to tmp
-gulp.task('html', function () {
+gulp.task('html', () => {
     return gulp.src(paths.srcHTML)
         .pipe(gulp.dest(paths.tmp));
 });
 // CSS output to tmp
-gulp.task('css', function () {
+gulp.task('css', () => {
     return gulp.src(paths.srcCSS)
         .pipe(gulp.dest(paths.tmp));
 });
 // JS output to tmp
-gulp.task('js', function () {
+gulp.task('js', () => {
     return gulp.src(paths.srcJS)
+        .pipe(babel())                      // transpiles js
+        .pipe(terser())                     // minifies js
+        .pipe(size({ title: 'scripts' }))   // logs file size
         .pipe(gulp.dest(paths.tmp));
 });
 // Copy task runs html/css/js to tmp tasks
 gulp.task('copy', gulp.series('html', 'css', 'js'));
 // Empty output directories with del
-gulp.task('clean', function () {
+gulp.task('clean', () => {
     del(['tmp/*', 'dist/*']);
 });
 
@@ -48,7 +54,7 @@ gulp.task('clean', function () {
 gulp.task('default', gulp.series('copy', 'js'));
 
 // Serve site and watch js
-gulp.task('serve', function () {
+gulp.task('serve', () => {
     browserSync.init({
         server: paths.tmp,
         port: 8000
@@ -61,7 +67,7 @@ gulp.task('serve', function () {
 gulp.task('build:serve', gulp.series('copy', 'js', 'serve'));
 
 // Series 'js-watch' after 'js' completes before browserSync reload
-gulp.task('js-watch', gulp.series('js'), function (done) {
+gulp.task('js-watch', gulp.series('js'), (done) => {
     browserSync.reload();
     done();
 });
